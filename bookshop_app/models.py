5a7adaa70acfcd3250cdc71f6377d4831416f270
Daniil INQ <danyagrasimov@gmail.com>
2023-03-24 06:48:59 +0100

    better models & settings

--- bookshop_app/models.py
+++ bookshop_app/models.py
@@ -1,20 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+
 class SpecialOffers(models.Model):
+    pass
 
 
-class Books(models.Model):
-    title = models.CharField(max_length=30, blank=False)
-    author = models.CharField(max_length=30, blank=False)
-    price = models.IntegerField()
-    cover = models.FileField(upload_to='uploads/')
-    description = models.CharField(max_length=200, blank=False)
-    publish_date = models.CharField(max_length=20)
 class Authors(models.Model):
     name = models.CharField(max_length=30)
     about = models.CharField(max_length=400)
 
-class Cart(models.Model):
-    pass
+
+class Books(models.Model):
+    title = models.CharField(max_length=30, blank=False)
+    author = models.ManyToManyField(Authors)
+    price = models.IntegerField()
+    cover = models.ImageField(upload_to='uploads/')
+    description = models.CharField(max_length=200, blank=False)
+    publish_date = models.CharField(max_length=20)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    creation_date = models.DateField(auto_now_add=True)
+    closed = models.BooleanField(default=False)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Books, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
--- bookshop_app/urls.py
+++ bookshop_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from bookshop_app.views import *
 
 urlpatterns = [
 
