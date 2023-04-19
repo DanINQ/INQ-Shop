730d565a4c1aa156f058d35968781df8c7e5be5b
Daniil INQ <danyagrasimov@gmail.com>
2023-04-19 08:00:13 +0200

    more templates

--- bookshop_app/urls.py
+++ bookshop_app/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from bookshop_app.views import *
 
 urlpatterns = [
+    path('', home_page, name='home'),
+    path('cart/', cart, name='cart'),
+    path('auth/', login, name='login'),
+    path('sign-up/', sign_up_page, name='sign-up')
 ]
--- bookshop_app/views.py
+++ bookshop_app/views.py
@@ -4,9 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def main_page(request):
+def home_page(request):
 	return render(request, 'main.html')
 
 
 def cart(request):
 	return render(request, 'cart.html')
+
+
+def login(request):
+	return render(request, 'login.html')
+
+
+def sign_up_page(request):
+	return render(request, 'sign-up.html')
